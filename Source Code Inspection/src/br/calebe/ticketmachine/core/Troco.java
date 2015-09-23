2b54864f9c3dd6d274a5c2b6b1ba29782755d6b2
thomazpicelli <thomazpicelli@gmail.com>
2015-09-23 11:47:34 -0300

    Alteração indice + remoção cont = 0;

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -17,31 +17,26 @@ class Troco {
             count++;
         }
         papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
         while (valor % 50 != 0) {
             count++;
         }
         papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
         while (valor % 20 != 0) {
             count++;
         }
         papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
         while (valor % 10 != 0) {
             count++;
         }
         papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
         while (valor % 5 != 0) {
             count++;
         }
         papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
         while (valor % 2 != 0) {
             count++;
         }
-        papeisMoeda[1] = new PapelMoeda(2, count);
+        papeisMoeda[0] = new PapelMoeda(2, count);
     }
 
     public Iterator<PapelMoeda> getIterator() {
