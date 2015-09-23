83f0f556fc308a22261e993e5e05c4fd9541d19c
thomazpicelli <thomazpicelli@gmail.com>
2015-09-23 11:41:52 -0300

    Inclusão dos construtores com a mesagem de exceção

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,5 +5,8 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
-    
+
+    public PapelMoedaInvalidaException() {
+        System.out.println("Moeda Inválida!");
+    }
 }
--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,8 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+
+    public SaldoInsuficienteException() {
+        System.out.println("Saldo Insuficiente");
+    }
 }
